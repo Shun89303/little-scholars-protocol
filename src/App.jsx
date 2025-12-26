169e694af4e3b3e2a5b7aa6ee17ba70d84f462f4
Shun <ruka4662@gmail.com>
2025-12-26 17:30:04 +0630

    initial nav bar setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Little Scholars Protocol</h1>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+        <div className="nav-container">
+            <div
+                className={`nav-icon ${open ? "open" : ""}`}
+                onClick={() => setOpen(!open)}
+            >
+                ☰
+            </div>
+            <div className={`nav-menu ${open ? "show" : ""}`}>
+                <a href="#">Dashboard</a>
+                <a href="#">Students</a>
+                <a href="#">Classes</a>
+                <a href="#">Teachers</a>
+                <a href="#">Settings</a>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
